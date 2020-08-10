e709ca4402df5bbfba9c367e1b6474701da89d66
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 01:05:15 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,14 +14,14 @@ require 'pry'
 #  }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
-      end
+  freddy = contacts["Freddy Mercury"]
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
     end
   end
-end
+  contacts
+end 
 
 
   
