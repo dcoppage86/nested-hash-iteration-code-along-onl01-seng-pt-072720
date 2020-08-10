cf4e144885976b2e55855f6d1b1ef7639d131430
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 01:12:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,7 +13,7 @@ require 'pry'
 #    }
 #  }
   
-
+def remove_strawberry
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
