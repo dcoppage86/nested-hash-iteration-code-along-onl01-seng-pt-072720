8f51e8abec5649078d438b2403730f291b919456
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 01:03:04 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,8 +13,14 @@ require 'pry'
 #    }
 #  }
   
-contacts.each do |person, contact_details_hash|
-  binding.pry
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_icecream_flavors
+        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
 
 
